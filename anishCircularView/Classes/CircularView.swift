7ee3ad324e49fb455304f81331322aa4ccecc74a
anish <anishakd4@gmail.com>
2019-07-19 15:09:28 +0530

    added simple circular uiview contrl

---
+++ anishCircularView/Classes/CircularView.swift
@@ -0,0 +1,27 @@
+//
+//  CircularButton.swift
+//  ReusableFramework
+//
+//  Created by Anish Kumar Dubey on 18/07/19.
+//  Copyright © 2019 Anish Kumar Dubey. All rights reserved.
+//
+
+import UIKit
+
+public class CircularView: UIView {
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    private func setupView(){
+        self.layer.cornerRadius = self.bounds.width/2
+        self.layer.masksToBounds = true
+        self.clipsToBounds = true
+    }
+    
+}
